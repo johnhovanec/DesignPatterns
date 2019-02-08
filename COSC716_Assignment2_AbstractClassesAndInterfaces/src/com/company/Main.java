344fc8aff2725f629945c363521e869e4517c239
jhovan1 <jahovanec@gmail.com>
2019-02-07 23:27:56 -0500

    Add interface and abstract class

--- COSC716_Assignment2_AbstractClassesAndInterfaces/src/com/company/Main.java
+++ COSC716_Assignment2_AbstractClassesAndInterfaces/src/com/company/Main.java
@@ -5,4 +5,40 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public interface AddrLabelInterface {
+        String getAttnName();
+        String getTitle(); // Mr., Mrs., etc.
+        String getName();
+        String getNameSuffix(); // e.g., Jr., III
+        String getProfessionalSuffix(); // O.D. String getStreet();
+        String getSuiteNum();
+        String getCity();
+        String getState();
+        String getZipCode();
+    }
+
+    public abstract class AddrLabel implements AddrLabelInterface {
+        public String getAttnName() {
+            return "";
+        }
+
+        public String getTitle() {
+            return "";
+        }
+
+        public String getNameSuffix() {
+            return "";
+        }
+
+        public String getProfessionalSuffix() {
+            return "";
+        }
+
+        public String getSuiteNum() {
+            return "";
+        }
+
+    }
 }
+
