9e6ab5438a6221e4dae27836d7d9fadd3883ee08
jhovan1 <jahovanec@gmail.com>
2019-03-08 22:40:54 -0500

    Add starter files

---
+++ Assignment4_CommandPattern/src/Commands/CMDGetMenu.java
@@ -0,0 +1,19 @@
+package Commands;
+
+import Aggregator.*;
+
+public class CMDGetMenu {
+    
+    private Aggregator agg;
+    
+    public CMDGetMenu(Aggregator agg){
+        this.agg = agg;
+    }
+    
+    public Object execute(){
+    return new Menu(agg.getMenu()); // if copy constructor implemented
+    // or
+    // return agg.getMenu().clone(); // if clone method implemented 
+    }
+    
+}
---
+++ Assignment4_CommandPattern/src/basicrestaurantsystem/BasicRestaurantSystem.java
@@ -0,0 +1,9 @@
+package basicrestaurantsystem;
+
+public class BasicRestaurantSystem {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
