a81ac2680c8f3132d486885a3f556c550bccb89a
jhovan1 <jahovanec@gmail.com>
2019-03-27 22:24:40 -0400

    Clean-up check-in

--- Assignment4_CommandPattern/src/Commands/Invoker.java
+++ Assignment4_CommandPattern/src/Commands/Invoker.java
@@ -5,11 +5,9 @@ import Aggregator.*;
 public class Invoker {
     private static Aggregator agg = new Aggregator();
     
-    
     public static Menu getMenu(){
         return (Menu) new CMDGetMenu(agg).execute();
     }
-
 
 	public static Tab getTab() {
 		return (Tab) new CMDGetTab(agg).execute();
